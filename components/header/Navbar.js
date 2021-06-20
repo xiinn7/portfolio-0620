74ddb13bf5cfc595b664836c5878bb984c56416f
陳榕馨 <ruby881017@gms.tku.edu.tw>
2021-06-20 23:21:54 +0800

    fetch data & auto create cards

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,29 +1,30 @@
 import styles from './Navbar.module.scss'
+import Link from 'next/link'
 
 export default function layout() {
     return (
         <div>
             <nav className={styles["menu"]}>
   <ol>
-    <li className={styles["menu-item"]}><a href="#0">Home</a></li>
-    <li className={styles["menu-item"]}><a href="#0">About</a></li>
+    <li className={styles["menu-item"]}><Link href='/'><h1>Home</h1></Link></li>
+    <li className={styles["menu-item"]}><Link href='/'><h1>About</h1></Link></li>
     <li className={styles["menu-item"]}>
-      <a href="#0">Menu</a>
+    <Link href='/'><h1>Menu</h1></Link>
       <ol className={styles["sub-menu"]}>
-        <li className={styles["menu-item"]}><a href="#0">Big Widgets</a></li>
-        <li className={styles["menu-item"]}><a href="#0">Bigger Widgets</a></li>
-        <li className={styles["menu-item"]}><a href="#0">Huge Widgets</a></li>
+        <li className={styles["menu-item"]}><a>Big Widgets</a></li>
+        <li className={styles["menu-item"]}><a >Bigger Widgets</a></li>
+        <li className={styles["menu-item"]}><a >Huge Widgets</a></li>
       </ol>
     </li>
     <li className={styles["menu-item"]}>
-      <a href="#0">Kabobs</a>
+    <Link href='/'><h1>Kabobs</h1></Link>
       <ol className="sub-menu">
-        <li className={styles["menu-item"]}><a href="#0">Shishkabobs</a></li>
-        <li className={styles["menu-item"]}><a href="#0">BBQ kabobs</a></li>
-        <li className={styles["menu-item"]}><a href="#0">Summer kabobs</a></li>
+        <li className={styles["menu-item"]}><a>Shishkabobs</a></li>
+        <li className={styles["menu-item"]}><a >BBQ kabobs</a></li>
+        <li className={styles["menu-item"]}><a >Summer kabobs</a></li>
       </ol>
     </li>
-    <li className={styles["menu-item"]}><a href="#0">Contact</a></li>
+    <li className={styles["menu-item"]}><a >Contact</a></li>
   </ol>
 </nav>
         </div>
